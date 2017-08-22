2121daac48f476991c9d51b25b36d017d8bb82e6
SamihMustafa <samihmustafa@gmail.com>
2017-08-22 22:05:00 +0400

    readding

---
+++ app/src/main/java/com/testing/yelpgl/businessdetail/BusinessDetailContract.java
@@ -0,0 +1,18 @@
+package com.testing.yelpgl.businessdetail;
+
+/**
+ * Created by Ali on 20-Aug-17.
+ */
+
+public interface BusinessDetailContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+}
+
---
+++ app/src/main/java/com/testing/yelpgl/model/Business.java
@@ -0,0 +1,8 @@
+package com.testing.yelpgl.model;
+
+/**
+ * Created by Ali on 20-Aug-17.
+ */
+
+public class Business {
+}
